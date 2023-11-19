d7845c33b8d6931b61f556d7bf58dc42668404db
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-11-19 10:07:24 -0800

    print c

--- main.h
+++ main.h
@@ -1,45 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
-
-#define BUFF_SIZE 1024
-#define UNUSED(x) (void)(x)
-
-
-/* FLAGS */
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_MINUS 1
-#define F_SPACE 16
-#define F_HASH 8
-
-/* SIZES */
-#define S_SHORT 1
-#define S_LONG 2
-
-/**
- * struct fmt - Structure representing a format.
- *
- * @fmt: The format specifier.
- * @fn: The associated function.
- */
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-
-/**
- * typedef struct fmt_t - Type def. for  the format structure.
- *
- * @fmt: The format specifier.
- * @fm_t: The associated function.
- */
-typedef struct fmt fmt_t;
-
+int _strlen(char *str);
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
