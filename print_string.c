d63c42a892412faae43278b0345df3c6b158a9fd
PKgothatso <ksp331@outlook.com>
2023-11-21 03:28:18 -0800

    changes

--- print_string.c
+++ print_string.c
@@ -1,29 +1,32 @@
 #include "main.h"
+
 /**
- * print_s - print a string.
- * @val: argumen t.
- * Return: the length of the string.
+ * _strlen - finds the lenght of a string.
+ * @s: string
+ * Return: integer.
  */
 
-int print_s(va_list val)
+int _strlen(char *s)
 {
-	char *s;
-	int i, len;
+	int c;
 
-	s = va_arg(val, char *);
-	if (s == NULL)
-	{
-		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
-	{
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
+
+/**
+ * _strlenc - finds length of a string but for constant characters
+ * @s: string
+ * Return: integer
+ */
+
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
 }
