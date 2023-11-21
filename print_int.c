d49222e714bae30b62a96d76b02668a87f597636
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-11-21 10:41:05 -0800

    Education is when you read the fine print.Experience is what you get if you don't

---
+++ print_int.c
@@ -0,0 +1,91 @@
+#include "main.h"
+/**
+ * print_i - Integer to be printed
+ * @args: Argument to be printed
+ * Return: Integer value.
+ */
+int print_i(va_list args)
+{
+	int n = va_args(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+
+			putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	putchar(last + '0');
+
+	return (i);
+}
+
+/**
+ * print_d - Decimal to be printed
+ * @args: Argument to be printed
+ * Return: Integer value
+ */
+int print_d(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+
+			putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	putchar(last + '0');
+
+	return (i);
+}
